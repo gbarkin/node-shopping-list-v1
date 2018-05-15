5fbdb8d0de7c9d0cd923710f658dca2ec29e0cde
George Barkin <george.barkin@gmail.com>
2018-05-14 20:25:45 -0400

    server.js update

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
-
 const express = require('express');
 // we'll use morgan to log the HTTP layer
 const morgan = require('morgan');
-// we'll use body-parser's json() method to 
+// we'll use body-parser's json() method to
 // parse JSON data sent in requests to this app
 const bodyParser = require('body-parser');
 
 // we import the ShoppingList model, which we'll
 // interact with in our GET endpoint
-const {ShoppingList} = require('./models');
+const {
+  ShoppingList
+} = require('./models');
+const {
+  Recipes
+} = require('./models');
+
 
 const jsonParser = bodyParser.json();
 const app = express();
@@ -17,7 +22,7 @@ const app = express();
 app.use(morgan('common'));
 
 // we're going to add some items to ShoppingList
-// so there's some data to look at. Note that 
+// so there's some data to look at. Note that
 // normally you wouldn't do this. Usually your
 // server will simply expose the state of the
 // underlying database.
@@ -29,6 +34,15 @@ ShoppingList.create('peppers', 4);
 // all current ShoppingList items by calling `ShoppingList.get()`
 app.get('/shopping-list', (req, res) => {
   res.json(ShoppingList.get());
+});
+
+Recipes.create('Burrito Bowl', ['steak', 'salsa', 'sour cream']);
+Recipes.create('Cereal', ['milk', 'Cereal']);
+
+app.get('/recipes', (req, res) => {
+
+  res.json(Recipes.get());
+
 });
 
 app.listen(process.env.PORT || 8080, () => {
